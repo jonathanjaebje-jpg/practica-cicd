3b0f6bceb06cf256a5b9afacba805dcbb2d61509
jonathanjaebje-jpg <jonathan.jaeb.je@gmail.com>
2026-09-23 16:19:06 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const assert = require("assert");
 
 // Prueba unitaria simple
 console.log("Ejecutando pruebas...");
-assert.strictEqual(1 + 1, 2, "La suma básica falló");
+assert.strictEqual(1 + 1, 3, "Error provocado intencionalmente");
 console.log("¡Todas las pruebas pasaron correctamente!");
