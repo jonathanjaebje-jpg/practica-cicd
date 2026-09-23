025991173ddfa5418c036ddbc440ccb84b8024c7
jonathanjaebje-jpg <jonathan.jaeb.je@gmail.com>
2026-09-23 15:45:07 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+const assert = require("assert");
+
+// Prueba unitaria simple
+console.log("Ejecutando pruebas...");
+assert.strictEqual(1 + 1, 2, "La suma básica falló");
+console.log("¡Todas las pruebas pasaron correctamente!");
